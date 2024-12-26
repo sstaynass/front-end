4175a0fcf8f79644ac484f8194f495917b22cf20
Tayna <taynasoares203@gmail.com>
2024-12-26 11:17:00 -0300

    primeiros exercicios de javascript

---
+++ javascript/javascript_primeiros exercicios/java.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    /* Ínicio do seu código */
+    function greetMe(seuNome) {
+      alert(`Olá ${seuNome}`);
+    }
+  
+    greetMe("Mundo");
+    /* Fim do seu código */
+  })();
